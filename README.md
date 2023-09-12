5d6b75133aabc8b57b6319d2d1a45b104c8b78d5
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-09-12 08:20:28 -0300

    bullmq

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # aws-ecs-redis-cluster
+
+
+```
+docker run -d --name redis-local -p 6379:6379 redis
+```
--- nodejs/index.ts
+++ nodejs/index.ts
@@ -1,16 +1,61 @@
+import 'dotenv/config'
 import express from 'express';
+import { Queue, Worker } from 'bullmq';
 
-const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('OK')
-})
+(async () => {
+  try {
+    require('dotenv').config();
 
-app.get('/health', (req, res) => {
-  res.send('OK')
-})
+    const REDIS_HOST = process.env.REDIS_HOST;
+    const REDIS_PORT = Number(process.env.REDIS_PORT);
+    const REDIS_CONNECT_TIMEOUT = Number(process.env.REDIS_CONNECT_TIMEOUT);
+    const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
+    const REDIS_TLS = process.env.REDIS_TLS;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    const app = express();
+    const port = 3000;
+
+    const tls = REDIS_TLS === "1" ? {} : undefined;
+    const queueName = "test-queue"
+
+    const myQueue = new Queue(queueName, {
+      connection: {
+        host: REDIS_HOST,
+        port: REDIS_PORT,
+        connectTimeout: REDIS_CONNECT_TIMEOUT,
+        tls: tls
+      }
+    });
+
+    const myWorker = new Worker(queueName, async (job) => { console.log(job.data) }, {
+      connection: {
+        host: REDIS_HOST,
+        port: REDIS_PORT,
+        connectTimeout: REDIS_CONNECT_TIMEOUT,
+        password: REDIS_PASSWORD,
+        tls: tls
+      }
+    });
+
+    app.get('/', (req, res) => {
+      res.send('OK')
+    })
+
+    app.post('/enqueue', async (req, res) => {
+      await myQueue.add('myJobName', { foo: 'bar' });
+      res.send('OK')
+    })
+
+    app.get('/health', (req, res) => {
+      res.send('OK')
+    })
+
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+    })
+  } catch (e) {
+    console.error(e);
+  }
+})();
+
